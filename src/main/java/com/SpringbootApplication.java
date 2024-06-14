bcc7273a8aad4bbe7f2ca15334a2665857eb737a
Lenovo <2478692577@qq.com>
2024-06-14 09:24:49 +0800

    lzq第一次提交：评价    第二次提交：留言、申请

--- src/main/java/com/SpringbootApplication.java
+++ src/main/java/com/SpringbootApplication.java
@@ -1,5 +1,7 @@
 package com;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,16 +19,23 @@ import java.io.IOException;
 @MapperScan("com.dao")
 public class SpringbootApplication extends SpringBootServletInitializer {
 
+    private static Logger log = LoggerFactory.getLogger(SpringbootApplication.class);
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SpringbootApplication.class);
         Environment env = context.getEnvironment();
-                try {
+        log.info("====================================================================");
+        log.info("项目版本: {}", env.getProperty("project.version"));
+        log.info("启动环境: {}", env.getProperty("project.env"));
+        log.info("启动端口: {}", env.getProperty("server.port"));
+        log.info("日志等级: {}", env.getProperty("logback.level"));
+        log.info("日志Appender: {}", env.getProperty("logback.appender"));
+        log.info("Startup complete ...");
+        log.info("====================================================================");
+        try {
             Runtime.getRuntime().exec("cmd /c start http://localhost:" + env.getProperty("server.port") +env.getProperty("server.servlet.context-path")+"/front/dist/index.html");
-//            Runtime.getRuntime().exec("cmd /c start http://localhost:" + env.getProperty("server.port") +env.getProperty("server.servlet.context-path")+"/admin/dist/index.html");
         } catch (IOException e) {
-            e.printStackTrace();
-
+            throw new RuntimeException(e);
         }
     }
 
